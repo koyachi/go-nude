699a88f33605865928ccd8cdd39bbce80fb1aaef
koyachi <rtk2106@gmail.com>
2015-04-10 22:47:30 +0900

    refactor: remove redundant code. Thanks @dchapes! https://github.com/koyachi/go-nude/commit/903ffad76c46a2b67e83101a0cf1b04b960899c3#commitcomment-9696856

--- util.go
+++ util.go
@@ -1,13 +1,11 @@
 package nude
 
 import (
-	"errors"
 	"image"
-	"image/gif"
-	"image/jpeg"
-	"image/png"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 	"os"
-	"strings"
 )
 
 // experimental
@@ -22,17 +20,6 @@ func decodeImage(filePath string) (img image.Image, err error) {
 	}
 	defer reader.Close()
 
-	last3Strings := strings.ToLower(filePath[len(filePath)-3:])
-	last4Strings := strings.ToLower(filePath[len(filePath)-4:])
-	if last3Strings == "jpg" || last4Strings == "jpeg" {
-		img, err = jpeg.Decode(reader)
-	} else if last3Strings == "gif" {
-		img, err = gif.Decode(reader)
-	} else if last3Strings == "png" {
-		img, err = png.Decode(reader)
-	} else {
-		img = nil
-		err = errors.New("unknown format")
-	}
+	img, _, err = image.Decode(reader)
 	return
 }
